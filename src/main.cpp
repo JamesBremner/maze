698c98fc82ae5db52cfde6be41613aaa1df058b3
James Bremner <james@ravenspoint.com>
2020-02-05 10:20:34 -0500

    input file from command line option

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,51 @@
 #include <iostream>
 #include <vector>
-
+#include <boost/program_options.hpp>
 #include "cMaze.h"
 
 using namespace std;
 
-int main()
+std::string Parse( int argc, char *argv[] )
+{
+    namespace po = boost::program_options;
+
+    // Declare the supported options.
+    po::options_description desc("Allowed options");
+    desc.add_options()
+    ("help", "produce help message")
+    ("input",   po::value<std::string>(), "input file.")
+    ;
+
+    // parse the command line
+    po::variables_map vm;
+    try
+    {
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+    }
+    catch (exception& e)
+    {
+        cerr << "error: " << e.what() << "\n";
+        exit(1);
+    }
+
+    if (vm.count("help"))
+    {
+        cout << desc << "\n";
+        exit(1);
+    }
+
+    if( vm.count("input") )
+    {
+        return vm["input"].as<std::string>();
+    }
+    return "";
+}
+
+int main( int argc, char* argv[] )
 {
     cMaze theMaze;
-    theMaze.read("maze2.txt");
+    theMaze.read( Parse( argc, argv ) );
 
     for( auto& s : theMaze.displayText() )
         cout << s << "\n";
